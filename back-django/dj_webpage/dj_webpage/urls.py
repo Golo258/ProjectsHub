97b11bccadc600f31fe61fdd1a2a0d0d37e6c145
Golon258 <golon338@gmail.com>
2024-05-14 20:31:50 +0200

    [VD] Add password reset by sending email

--- back-django/dj_webpage/dj_webpage/urls.py
+++ back-django/dj_webpage/dj_webpage/urls.py
@@ -1,13 +1,11 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views as user_views
 from users import forms as user_forms
 
-from django.conf.urls.static import  static
+from django.conf.urls.static import static
 from django.conf import settings
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,7 +14,18 @@ urlpatterns = [
     path('login/', user_forms.LoginFormView.as_view(), name="login"),
     path('logout/', user_views.logout_view, name='logout'),
     path('profile/', user_views.profile, name="profile"),
-
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
